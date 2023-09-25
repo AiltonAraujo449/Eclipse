f6db2e1b1bcb1a134967fecd7fae6aadbbc2c7f3
AiltonAraujo449 <ailtonaraujo449@gmail.com>
2023-09-25 15:23:34 -0300

    Adicionei novas linhas no código.

--- Hello GitHub/src/br/com/ailtonaraujo449/git/HelloGit.java
+++ Hello GitHub/src/br/com/ailtonaraujo449/git/HelloGit.java
@@ -13,8 +13,8 @@ public class HelloGit {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello GitHub");
-
+		System.out.println("Hello GitHub novamente");
+		System.out.println("Ailton Araujo");
 	}
 
 }
