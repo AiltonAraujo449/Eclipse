b4f8a85e3b4ea58e9741c029b08b498380f8b7fa
Ailton Alves de Araujo <84590850+AiltonAraujo449@users.noreply.github.com>
2023-09-25 15:13:47 -0300

    Adicionei arquivos via upload.

---
+++ Hello GitHub/src/br/com/ailtonaraujo449/git/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.ailtonaraujo449.git;
+
+/**
+ * @author Ailton Araujo
+ * Exemplo de Integração do Eclipse
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello GitHub");
+
+	}
+
+}
